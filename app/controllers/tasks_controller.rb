33bfdad22bcc8dd2565b742e5e0b4e89f649d428
sebastian carr <seb.carr@icloud.com>
2023-09-12 22:10:17 +0200

    rails task manager with no CSS

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,40 +1,42 @@
 class TasksController < ApplicationController
   def index
-    # Your code for listing tasks goes here
+    @tasks = Task.all
   end
 
-  # GET /tasks/1
   def show
-    # Your code for displaying a single task goes here
+    @task = Task.find(params[:id])
   end
 
-  # GET /tasks/new
   def new
-    # Your code for displaying the form to create a new task goes here
+    @task = Task.new
   end
 
-  # POST /tasks
   def create
-    # Your code for creating a new task goes here
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
   end
 
-  # GET /tasks/1/edit
   def edit
-    # Your code for displaying the form to edit an existing task goes here
+    @task = Task.find(params[:id])
   end
 
-  # PATCH/PUT /tasks/1
   def update
-    # Your code for updating an existing task goes here
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path(@task)
   end
 
-  # DELETE /tasks/1
   def destroy
-    # Your code for deleting a task goes here
+    @task = Task.find(params[:id])
+    @task.destroy
+    # No need for app/views/restaurants/destroy.html.erb
+    redirect_to tasks_path, status: :see_other
   end
 
   private
 
-  # Define any private methods or helper methods here
-
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
