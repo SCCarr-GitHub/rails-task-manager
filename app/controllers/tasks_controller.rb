80319a31c86c398c894fd8cf4435d551c1c4e538
sebastian carr <seb.carr@icloud.com>
2023-09-12 12:27:55 +0200

    added task model added task model

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,40 @@
+class TasksController < ApplicationController
+  def index
+    # Your code for listing tasks goes here
+  end
+
+  # GET /tasks/1
+  def show
+    # Your code for displaying a single task goes here
+  end
+
+  # GET /tasks/new
+  def new
+    # Your code for displaying the form to create a new task goes here
+  end
+
+  # POST /tasks
+  def create
+    # Your code for creating a new task goes here
+  end
+
+  # GET /tasks/1/edit
+  def edit
+    # Your code for displaying the form to edit an existing task goes here
+  end
+
+  # PATCH/PUT /tasks/1
+  def update
+    # Your code for updating an existing task goes here
+  end
+
+  # DELETE /tasks/1
+  def destroy
+    # Your code for deleting a task goes here
+  end
+
+  private
+
+  # Define any private methods or helper methods here
+
+end
